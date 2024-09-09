37e3a06826f822d8b2918e0e1d019c7c667cf057
kaveripunu <kaveripunu@gmail.com>
2024-09-09 23:33:14 +0545

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+  return (
+    <div>
+      <Button className='bg-orange hover:bg-hoverOrange'>Lets make a button</Button>
+    </div>
+  )
+}
+
+export default App
